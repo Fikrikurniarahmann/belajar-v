d4a60db44bb3dcf099104bb07bd68b6f007cfff2
fikrikurniarahmann <fikrikurniarahman.17@gmail.com>
2023-03-28 14:59:08 +0700

    Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="" class="logo" src="https://ae01.alicdn.com/kf/S0793b2867ea244e5be309ab3fc9c4ab4E/Stiker-Mobil-Warna-warni-Kepala-Banteng-Marah-Lucu-Stiker-PVC-Aksesori-Eksterior-Sepeda-Motor-Mobil-untuk.jpg" width="125" height="125"/>
+    <div class="wrapper">
+      <HelloWorld msg="BANTENG KU GANTENG" />
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
